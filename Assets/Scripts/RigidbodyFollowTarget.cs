225e7995a2c6eff43e6a251f543e4f1578f61e91
home <dashiell.corning@mymail.champlain.edu>
2022-10-05 03:27:14 -0400

    Fixed Hand Rotation
    Thank gosh

--- Assets/Scripts/RigidbodyFollowTarget.cs
+++ Assets/Scripts/RigidbodyFollowTarget.cs
@@ -10,6 +10,7 @@ public class RigidbodyFollowTarget : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _rb.maxAngularVelocity = 20f;
     }
 
     // Update is called once per frame
@@ -19,10 +20,28 @@ public class RigidbodyFollowTarget : MonoBehaviour
 
         Quaternion rotationDifference = _targ.rotation * Quaternion.Inverse(transform.rotation);
 
-        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
-        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+        //rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+
+        Vector3 rotationDifferenceInDegree = ClampEulerAngelsToClosest(new Vector3(rotationDifference.eulerAngles.x, rotationDifference.eulerAngles.y, rotationDifference.eulerAngles.z));
 
         _rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
 
+    }
+    private Vector3 ClampEulerAngelsToClosest(Vector3 rotation)
+    {
+        if (rotation.x > 180)
+        {
+            rotation.x = rotation.x - 360;
+        }
+        if (rotation.y > 180)
+        {
+            rotation.y = rotation.y - 360;
+        }
+        if (rotation.z > 180)
+        {
+            rotation.z = rotation.z - 360;
+        }
+        return rotation;
     }
 }
